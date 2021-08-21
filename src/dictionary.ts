d01d8b1d57ab785a2ea94e9cdee7321064aa8bda
maddin1502 <maddin1502@users.noreply.github.com>
2021-08-21 22:13:43 +0200

    Revert "base config; dictionary, constructor"
    This reverts commit db33f819f9a0725a1c96ef823499fd16d4654a31.

--- src/dictionary.ts
+++
@@ -1,6 +0,0 @@
-export type Dictionary<T = any, K extends string | number = string> = {
-  [key in K]?: T;
-}
-export type ReadonlyDictionary<T = any, K extends string | number = string> = Readonly<Dictionary<T,K>>
-export type DictionaryKey<D> = D extends Dictionary<any, infer P> ? P : never;
-export type DictionaryValue<D> = D extends Dictionary<infer P> ? P : never;
