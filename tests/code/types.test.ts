08c9edd726f40724f5a2815aa03d6ebacf15e9a3
maddin1502 <maddin1502@users.noreply.github.com>
2024-04-13 17:09:26 +0200

    add dummy tests for types

---
+++ tests/code/types.test.ts
@@ -0,0 +1,13 @@
+import '@/array.js';
+import '@/constructor.js';
+import '@/dictionary.js';
+import '@/emptyObject.js';
+import '@/event/types.js';
+import '@/mapping.js';
+import { describe, expect, test } from 'vitest';
+
+describe('dummy types test', () => {
+  test('dummy', () => {
+    expect(true).toBe(true);
+  });
+});
