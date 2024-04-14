fbb9d702e80693a65b11de20db2c385ad93e3adc
maddin1502 <maddin1502@users.noreply.github.com>
2024-04-14 18:37:16 +0200

    like X types

--- tests/code/types.test.ts
+++ tests/code/types.test.ts
@@ -3,6 +3,7 @@ import '@/constructor.js';
 import '@/dictionary.js';
 import '@/emptyObject.js';
 import '@/event/types.js';
+import '@/likes.js';
 import '@/mapping.js';
 import { describe, expect, test } from 'vitest';
 
